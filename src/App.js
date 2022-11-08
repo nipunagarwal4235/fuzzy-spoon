0a75ed1379b7270185c1d1775249dfca5b8b5c3c
Sachinrawat1215 <sachinrawat0853@gmail.com>
2022-11-09 01:01:57 +0530

    resolved conflict

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import './CSS/App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <SubHead />
-=======
-      <h1 className="text-3xl font-bold underline">
-        Hello world! hitesh here
-        
-      </h1>
->>>>>>> e4f638249bd5e905aa54d7b4ceba6c85ce24f0dc
     </div>
   );
 }

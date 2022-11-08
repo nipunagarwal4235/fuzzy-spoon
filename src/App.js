06f30a8f1d88ea40a349fd28b2cee20c95a0431f
Hs809 <hiteshpal.8097@gmail.com>
2022-11-08 23:28:40 +0530

    checking for pr

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello world! hitesh here
+        
       </h1>
     </div>
   );

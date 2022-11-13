8c969308673f6f6c3cb42c65eb335b450a5653ae
Sachinrawat1215 <sachinrawat0853@gmail.com>
2022-11-13 15:49:38 +0530

    created footer with responsiveness

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './CSS/App.css';
 
 import Navbar from './components/navbar/Navbar';
 import Slider from './components/slider/Slider';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Slider />
+      <Footer />
     </div>
   );
 }
